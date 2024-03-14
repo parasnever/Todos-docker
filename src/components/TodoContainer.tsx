869d4d329165b6b8a7063eeed53e33f1268c94dc
paras from office <parasshresthanever@gmail.com>
2024-03-14 08:57:24 -0700

    changes of 30 min

--- src/components/TodoContainer.tsx
+++ src/components/TodoContainer.tsx
@@ -2,30 +2,29 @@
 
 
 export function TodoContainer() {
+const handleAddTodo = () =>{
+  
+}
 
-
-    //only for the setup of the query
-
-    // const {data , isLoading, error} = useQuery<{message: string}>({
-    //     queryKey : ['test'],
-    //     queryFn: async () =>{  
-    //         return new Promise((resolve)=>{
-    //             setTimeout(() => {
-    //                 resolve({message: "react query working"})
-    //             }, 2000);
-    //         })
-    //     }
-    // })
-    // if(isLoading){
-    //     return <p>Loading ....</p>
-    // }
-    // if(error){
-    //     return <p>Something is wrong : </p>
-    // }
   return (
     <div>
-        TodoContainer : 
-        {/* {data?.message} */}
+        <h1>My Todos </h1>
+        {/* form container */}
+        <div id="form_container">
+          <form onSubmit={handleAddTodo}>
+            {/* title */}
+          <label htmlFor="title" >
+            <input name="title"id="title"/>
+          </label>
+
+            {/* description */}
+           <label htmlFor="description">
+            <textarea name="description" id="description" ></textarea>
+           </label>
+           <button type="submit">Add</button>
+          </form>
+        </div>
+
         </div>
   )
 }
